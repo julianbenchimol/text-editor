3bec1493937e3bbfee7199730ceed95edc8d5201
Julian Benchimol <jbenchimol1999@gmail.com>
2023-02-24 10:58:56 -0500

    foxed package issues

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// DONE: Add logic to a method that accepts some content and adds it to the database
 
 export const postDb = async(content) =>{
   console.log('post to the database');
@@ -27,6 +26,7 @@ export const postDb = async(content) =>{
   console.log("Note Saved!", result)
 }
 
+// DONE: Add logic for a method that gets all the content from the database
 export const getDb = async() =>{
   console.log("getting database info");
 
@@ -40,7 +40,5 @@ export const getDb = async() =>{
   return result;
 }
 
-// TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,14 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// DONE: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
 
     butInstall.classList.toggle('hidden', false)
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// DONE: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
@@ -22,7 +22,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true)
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// DONE: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
